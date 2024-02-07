1cae3637194e0948ade264946f57fd59341d031d
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2024-02-07 12:42:59 +0900

    Add sitemap

--- build.ts
+++ build.ts
@@ -1,4 +1,12 @@
 import { toSSG } from "hono/bun";
+import { generateSitemap } from "sitemap-ts";
 import app from "./src/index";
 
 toSSG(app);
+
+const hostname = process.env.VITE_GIALOG_BASE_URL;
+generateSitemap({
+  outDir: "./static",
+  readable: true,
+  hostname,
+});
