4d451b16570aa6944776678d9bd21733b9cb5795
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2024-02-07 13:32:20 +0900

    Modify to execute generateSitemap after toSSG

--- build.ts
+++ build.ts
@@ -2,7 +2,7 @@ import { toSSG } from "hono/bun";
 import { generateSitemap } from "sitemap-ts";
 import app from "./src/index";
 
-toSSG(app);
+await toSSG(app);
 
 const hostname = process.env.VITE_GIALOG_BASE_URL;
 generateSitemap({
